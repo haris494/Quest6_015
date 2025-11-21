50107b9569ad203f6197d16bfe2cf09cffde91e0
haris49 <hariis.459@gmail.com>
2025-11-21 22:46:50 +0700

    expose StateFlow statusUI sebagai data yang dapat dibaca

--- app/src/main/java/com/example/navigasidengandata/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/navigasidengandata/viewmodel/SiswaViewModel.kt
@@ -3,7 +3,10 @@ package com.example.navigasidengandata.viewmodel
 import androidx.lifecycle.ViewModel
 import com.example.navigasidengandata.view.Siswa
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class SiswaViewModel : ViewModel (){
     private val _statusUI = MutableStateFlow(Siswa())
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
 }
