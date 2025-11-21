5ceabf1aded9808af19b148e57b51964daf55043
haris49 <hariis.459@gmail.com>
2025-11-21 22:03:05 +0700

    tambah state txtNama txtAlamat txtGender

--- app/src/main/java/com/example/navigasidengandata/view/Formulir.kt
+++ app/src/main/java/com/example/navigasidengandata/view/Formulir.kt
@@ -2,6 +2,9 @@ package com.example.navigasidengandata.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -11,3 +14,11 @@ fun FormIsian(
     onSubmitButtonClicked : (MutableList<String>) -> Unit,
     modifier: Modifier = Modifier
 ) {
+
+    var txtNama by rememberSaveable{ mutableStateOf("")}
+    var txtAlamat by remember { mutableStateOf("")}
+    var txtGender by remember { mutableStateOf("")}
+    var listData: MutableList<String> = mutableListOf( txtNama,txtAlamat,txtGender)
+
+
+}
