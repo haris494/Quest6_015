3b8822c4284219b827b3173acce24a97a8ffecf4
haris49 <hariis.459@gmail.com>
2025-11-21 22:58:08 +0700

    tambah composable SiswaApp sebagai container navigasi

--- app/src/main/java/com/example/navigasidengandata/viewmodel/Navigasi.kt
+++ app/src/main/java/com/example/navigasidengandata/viewmodel/Navigasi.kt
@@ -1,6 +1,13 @@
 package com.example.navigasidengandata.viewmodel
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
 enum class Navigasi {
     Formulir,
     Detail
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaApp(
