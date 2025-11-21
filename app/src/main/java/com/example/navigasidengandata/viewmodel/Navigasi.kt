966a875c3d6e3550e67cd17713edc675c18469fc
haris49 <hariis.459@gmail.com>
2025-11-21 22:59:05 +0700

    tambahkan parameter modifier, viewModel, dan navController pada SiswaApp

--- app/src/main/java/com/example/navigasidengandata/viewmodel/Navigasi.kt
+++ app/src/main/java/com/example/navigasidengandata/viewmodel/Navigasi.kt
@@ -2,6 +2,10 @@ package com.example.navigasidengandata.viewmodel
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
     Formulir,
@@ -11,3 +15,7 @@ enum class Navigasi {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SiswaApp(
+    modifier: Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+    navController : NavHostController = rememberNavController()
+){
