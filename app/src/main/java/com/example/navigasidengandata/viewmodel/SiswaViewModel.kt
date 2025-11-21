11c766db8e16c92d2f15724b5bce06739b567532
haris49 <hariis.459@gmail.com>
2025-11-21 22:45:16 +0700

    MutableStateFlow untuk menyimpan status UI siswa

--- app/src/main/java/com/example/navigasidengandata/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/navigasidengandata/viewmodel/SiswaViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.navigasidengandata.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.navigasidengandata.view.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class SiswaViewModel : ViewModel (){
+    private val _statusUI = MutableStateFlow(Siswa())
 }
