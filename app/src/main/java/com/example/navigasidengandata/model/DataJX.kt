d95960945e30ee7c4d907e505b00e7b89c0fd310
haris49 <hariis.459@gmail.com>
2025-12-09 19:14:45 +0700

    menambahkan data jenis kelamin

---
+++ app/src/main/java/com/example/navigasidengandata/model/DataJX.kt
@@ -0,0 +1,11 @@
+package com.example.navigasidengandata.model
+
+import com.example.questnavigasitugas.R
+
+object DataJX {
+    val JenisK = listOf(
+        R.string.laki,
+        R.string.wanita
+
+    )
+}
